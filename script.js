de3050736502045eea7fcab4f4babbe48661ef08
rinky-maharjan <86604442+rinky-maharjan@users.noreply.github.com>
2023-10-09 09:02:28 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
-
-let string = "";
+let string = "";/*initialization*/
 let arr = Array.from(buttons);
 arr.forEach(button => {
     button.addEventListener('click', (e) =>{
@@ -9,12 +8,11 @@ arr.forEach(button => {
             string = eval(string);
             input.value = string;
         }
-
-        else if(e.target.innerHTML == 'AC'){
+        else if(e.target.innerHTML == 'AC'){/*all clear*/
             string = "";
             input.value = string;
         }
-        else if(e.target.innerHTML == 'DEL'){
+        else if(e.target.innerHTML == 'DEL'){/*delete*/
             string = string.substring(0, string.length-1);
             input.value = string;
         }
